ec2f2c03f176906f78ce7d0f24eef6636cd62eac
AriRainbow <mariel.williams@atlasschool.com>
2024-08-28 02:07:30 +0000

    5. Copy - Cut - Paste

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: "
+      f"{word[:3]}")
+print(f"Last 2 letters: "
+      f"{word[-2:]}")
+print(f"Middle word: "
+      f"{word[1:-1]}")
