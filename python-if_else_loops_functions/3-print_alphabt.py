3d0bc0c3fa3c10d78b0ac9c4d6543753e449968f
AriRainbow <mariel.williams@atlasschool.com>
2024-08-30 05:31:53 +0000

    Error: E128 continuation line under-indented for visual indent

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print("{}".format(
-                  "".join(chr(ascii_val)
-for ascii_val in range(97, 123)
-if chr(ascii_val) not in ('e', 'q'))), end="")
+    "".join(chr(ascii_val)
+            for ascii_val in range(97, 123)
+            if chr(ascii_val) not in ('e', 'q'))), end="")
