e2e7630a711735b3fc2a3989bad052590c700130
AriRainbow <mariel.williams@atlasschool.com>
2024-09-01 22:08:13 +0000

    Error: You can only use no more than 2 print functions with string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    # loop through string
+    result = ""
+
     for char in str:
-        # check if lowercase
         if 97 <= ord(char) <= 122:
-            # convert lowercase to uppercase
             char = chr(ord(char) - 32)
-        # avoid new lines
-        print("{}".format(char), end="")
-    # print new line after loop
-    print()
+        result += "{}".format(char)
+
+    print("{}".format(result))
