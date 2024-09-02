66ed8ddc240e1006bac908170a1a13c457e3d11e
AriRainbow <mariel.williams@atlasschool.com>
2024-09-02 20:35:53 +0000

    0. Import a simple function from a simple file mandatory

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# import
+from add_0 import add
+
+# assign values
+a = 1
+b = 2
+
+# calculate with add
+result = add(a,b)
+
+# print in format
+print("{} + {} = {}".format(a, b, result))
+
+#run only when executed directly
+if __name__ == "__main__":
+    pass
