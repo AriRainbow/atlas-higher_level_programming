edf917e57ab15b728ac982ad5e9c7a3b832f04cf
AriRainbow <mariel.williams@atlasschool.com>
2024-09-02 20:42:10 +0000

    Error: pycodestyle validation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,11 +7,11 @@ a = 1
 b = 2
 
 # calculate with add
-result = add(a,b)
+result = add(a, b)
 
 # print in format
 print("{} + {} = {}".format(a, b, result))
 
-#run only when executed directly
+# run only when executed directly
 if __name__ == "__main__":
     pass
