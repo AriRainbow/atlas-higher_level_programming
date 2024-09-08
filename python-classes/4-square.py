2c7b6137e7a24c894a7eaa028fbc8b0f685fb372
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-08 02:08:02 -0500

    Error: Pycodestyle validation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -31,7 +31,7 @@ class Square:
             int: The size of the square.
         """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Sets the size of the square.
