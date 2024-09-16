70045fdd91588261cca30856fbb2d36963bd469b
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-16 18:33:07 -0500

    0. Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    
+    Parameters:
+    obj (object): The object whose attributes and methods are to be listed.
+    
+    Returns:
+    list: A list of attributes and methods of the object.
+    """
+    return dir(obj)
