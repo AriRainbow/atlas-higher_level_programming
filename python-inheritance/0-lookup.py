5f6cf2771880ec77551b99743f870eb75704827b
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-16 19:01:11 -0500

    Error: PEP8 validation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,8 @@
 This module defines a finction lookup which
 returns the list of available attributes and methodsof an object.
 """
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
     return dir(obj)
