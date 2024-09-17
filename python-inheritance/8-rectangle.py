ef6c81fccd1d4031d5d8c0f4282e6caded75e41d
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-17 17:26:53 -0500

    Error: Module is documented

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,19 +2,44 @@
 """
 This module defines the Rectangle class, which inherits from the BaseGeometry class.
 It represents a rectangle with specific width and height attributes.
+
+Classes:
+    Rectangle: A class representing a rectangle, with width and height attributes
+                validated by the integer_validator method from BaseGeometry.
+                
+Attributes:
+    None
+    
+Methods:
+    __init__(self, width, height): Initializes a new Rectangle instance with the given
+                                    width and height, validated for positive integers.
 """
 
 BaseGeometry = __import__('7-_base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Rectangle class inheriting from BaseGeometry"""
+    """
+    Rectangle class inheriting from BaseGeometry.
+    
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+        
+    Methods:
+        __init(self, width, height): Initializes a new Rectangle instance.
+    """
 
     def __init__(self, width, height):
-        """Initializing a new Rectangle
+        """
+        Initializing a new Rectangle
  
         Args:
             width (int): The width of the rectangle
             height (int): The height of the rectangle
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
         """
         self.__width = width
         self.__height = height
