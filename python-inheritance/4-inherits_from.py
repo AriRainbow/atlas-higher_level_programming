cad8514f2c0e407448e7688fe924b6332acf62b3
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-17 15:04:55 -0500

    Error: PEP8 validation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,14 +7,16 @@ that inherited (directly or indirectly) from the specified class.
 
 def inherits_from(obj, a_class):
     """
-    Returns true if obj is an instance of a class that inherited (directly or indirectly)
+    Returns true if obj is an instance of a class
+    that inherited (directly or indirectly)
     from a_class. Otherwise, returns False.
 
     Args:
         obj: The object to check.
         a_class: The class to compare with.
-    
+
     Returns:
-        bool: True if obj is an instance of a subclass of a_class, excluding direct instancesof a_class.
+        bool: True if obj is an instance of a subclass of a_class,
+        excluding direct instancesof a_class.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and not type(obj) is a_class
