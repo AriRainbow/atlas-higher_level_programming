1da1aca9cc2506f5416b67662b69f1bc2a345f4e
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-17 03:46:00 -0500

    Error: PEP8 validation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,14 +7,16 @@ a given class or an instance of a class that inherited from the given class.
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if obj is an onstance of a_class or a class that inherited from a_class.
+    Returns True if obj is an onstance of a_class or a class
+    that inherited from a_class.
     Otherwise, returns False.
 
     Args:
         obj: The object to check.
         a_class: The class to compair with.
-    
+
     Returns:
-        bool: True if obj is an instance of, or inherited from a_class. False otherwise.
+        bool: True if obj is an instance of, or inherited from a_class.
+        False otherwise.
     """
     return isinstance(obj, a_class)
