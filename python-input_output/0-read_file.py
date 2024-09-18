e94fa2d26ed68e5447fe595d83f717b3c56caa28
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-17 19:59:08 -0500

    Error: PEP8 validation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a function that reads a text file (UTF8) and prints it to stdout.
+This module defines a function that reads a text file (UTF8)
+and prints it to stdout.
 """
 
 
@@ -9,7 +10,8 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+                        Defaults to an empty string.
     """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
