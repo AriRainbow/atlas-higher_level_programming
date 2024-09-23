c74bf0fe6839b49439027211520fd12419ed16f0
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-22 20:53:20 -0500

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,18 @@
->>>
+#!/usr/bin/python3
+"""
+Base class for managing id attribute.
+"""
+
+
+class Base:
+    """Represents the base model."""
+
+    __nb_objects = 0 # Private class attribute to keep track of the number of objects
+    
+    def __init__(self, id=None):
+        """Initialize the Base class with id or auto-increment."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
