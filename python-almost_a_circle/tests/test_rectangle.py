fedfe5a3460ae4c69f1a0e5d288c1d725687e07e
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-24 22:33:50 -0500

    Update: test_rectangle_creation_with_invalid_width

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -4,12 +4,21 @@ Unit tests for the Rectangle class.
 """
 import unittest
 Rectangle = __import__('models.rectangle', fromlist=['Rectangle']).Rectangle
+
+
 class TestRectangle(unittest.TestCase):
     """Test cases for the Rectangle class."""
+    
     def test_rectangle_creation(self):
         """Test Rectangle instantiation with valid parameters."""
         r = Rectangle(1, 2)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
+
+    def test_rectangle_creation_with_invalid_width(self):
+        """Test instantiation with an invalid width."""
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+
 if __name__ == "__main__":
     unittest.main()
