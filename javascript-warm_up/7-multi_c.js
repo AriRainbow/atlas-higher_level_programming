a6db51ed5dd1ed17fd9726b34623bb47453b2516
AriRainbow <mariel.williams@atlasschool.com>
2024-11-15 23:42:20 -0600

    7-multi_c.js, Executable

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Get the first argument passed to the script
+const x = parseInt(process.argv[2]);
+
+// Check if the argument is a valid integer
+if (isNaN(x) || x <= 0) {
+  console.log('Missing number of occurances');
+} else {
+  // Print "C is fun" x times using a loop
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
