d50c4b0cbd37118d77c656fc5e2f148bb7489fdc
AriRainbow <mariel.williams@atlasschool.com>
2024-11-15 23:20:14 -0600

    6-multi_languages_loop.js, executable

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Array of strings to print
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each element
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
