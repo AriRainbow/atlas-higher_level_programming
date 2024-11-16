c44251fd16409621b84c0c5181b60ce41b9f173a
AriRainbow <mariel.williams@atlasschool.com>
2024-11-15 20:57:31 -0600

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!usr/bin/node
+
+// Check the number of arguments passed
+const argsLength = process.argv.length - 2; // Exclude the first two elements (node and script name)
+
+// Print the corresponding message
+if (argsLength === 0) {
+    console.log('No argument');
+} else if (argsLength === 1) {
+    console.log('Argument Found');
+} else {
+    console.log('Argument found');
+}
