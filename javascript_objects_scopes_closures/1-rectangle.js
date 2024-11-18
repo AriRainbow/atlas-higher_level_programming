f1eed2b131213dcc0458a9588307370672318bb3
AriRainbow <mariel.williams@atlasschool.com>
2024-11-17 23:04:40 -0600

    1-rectangle.js, Executable

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // Constructor to initiate the rectangle with width and height
+  constructor (w, h) {
+    this.width = w; // Set the width attribute
+    this.height = h; // Set the height attribute
+  }
+}
+
+module.exports = Rectangle;
