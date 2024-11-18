e9e9db7671ac2601c0ec5d0644f9741a3ab97186
AriRainbow <mariel.williams@atlasschool.com>
2024-11-18 01:52:19 -0600

    Semistandard

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,6 @@
 const Square = require('./5-square'); // Import the Square class from 5-square.js
 
 class SquareExt extends Square {
-  // Constructor to initialize the square with size
-  constructor (size) {
-    super(size); // Call the constructor of Square (which calls Rectangle's constructor)
-  }
-
   // Method to print the square using the specified character (default is 'X')
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
