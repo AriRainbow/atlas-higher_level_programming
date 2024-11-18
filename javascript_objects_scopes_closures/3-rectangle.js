1da90084326104f4465227562966294248c6cc3f
AriRainbow <mariel.williams@atlasschool.com>
2024-11-18 01:21:55 -0600

    3-rectangle.js, Executable

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // Constructor to initialize the rectangle with width and height
+  constructor (w, h) {
+    // Check if w and h are positive integers greater than 0
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If dimensions are invalid, create an empty object
+      return {};
+    }
+  }
+
+  // Method to print the rectangle using 'X' characters
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
